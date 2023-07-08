a7bb194ba445a6d17139707facb5caea33ea1de2
Dogancan Mavideniz <dogancanmavideniz@gmail.com>
2023-07-08 11:25:53 +0300

    service added

--- Dev Tool AI/Network/Service.swift
+++ Dev Tool AI/Network/Service.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+
+class OpenAIAPIClient {
+    let apiKey: String
+    let session: URLSession
+    
+    init(apiKey: String) {
+        self.apiKey = apiKey
+        self.session = URLSession(configuration: .default)
+    }
+    
+    func sendMessage(_ message: String, completion: @escaping (Result<String, Error>) -> Void) {
+        let apiKey = "YOUR_API_KEY"
+        let endpoint = "https://api.openai.com/v1/engines/davinci-codex/completions"
+        
+        guard let url = URL(string: endpoint) else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Geçersiz URL"])))
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let parameters: [String: Any] = [
+            "prompt": message,
+            "max_tokens": 50
+        ]
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: parameters, options: []) else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Geçersiz parametreler"])))
+            return
+        }
+        
+        request.httpBody = httpBody
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Veri alınamadı"])))
+                return
+            }
+            
+            if let responseString = String(data: data, encoding: .utf8) {
+                completion(.success(responseString))
+            } else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Geçersiz yanıt"])))
+            }
+        }
+        
+        task.resume()
+    }
+
+}
+
+

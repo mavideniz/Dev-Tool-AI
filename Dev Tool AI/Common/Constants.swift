678409c50d39425cbaefd2b5f9694ac5b491d7ac
Dogancan Mavideniz <dogancanmavideniz@gmail.com>
2023-07-08 12:04:26 +0300

    openAI Key added

---
+++ Dev Tool AI/Common/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Dev Tool AI
+//
+//  Created by Doğancan Mavideniz on 8.07.2023.
+//
+
+import Foundation
+
+struct AppConstants {
+    static let openApiKey = "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"
+}
+

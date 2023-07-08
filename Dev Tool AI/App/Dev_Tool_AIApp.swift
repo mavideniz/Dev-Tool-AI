92a72ddd1a22cb98083ea8792385d3d060eac571
Dogancan Mavideniz <dogancanmavideniz@gmail.com>
2023-07-08 10:17:59 +0300

    init project

---
+++ Dev Tool AI/App/Dev_Tool_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dev_Tool_AIApp.swift
+//  Dev Tool AI
+//
+//  Created by Doğancan Mavideniz on 8.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dev_Tool_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

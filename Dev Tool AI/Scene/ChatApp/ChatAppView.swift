1a717df595b708388890f4b1a8df489f3e2530f8
Dogancan Mavideniz <dogancanmavideniz@gmail.com>
2023-07-08 11:25:47 +0300

    view changed for chatApp

--- Dev Tool AI/Scene/ChatApp/ChatAppView.swift
+++ Dev Tool AI/Scene/ChatApp/ChatAppView.swift
@@ -7,11 +7,52 @@
 
 import SwiftUI
 
+struct Message: Identifiable {
+    let id = UUID()
+    let content: String
+    let isSentByUser: Bool
+}
+
 struct ChatAppView: View {
+    @State private var messages: [Message] = []
+    @State private var newMessage: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 8) {
+                    ForEach(messages) { message in
+                        Text(message.content)
+                            .padding(8)
+                            .background(message.isSentByUser ? Color.blue : Color.gray)
+                            .foregroundColor(.white)
+                            .cornerRadius(8)
+                    }
+                }
+            }.padding(.top)
+            
+            HStack {
+                TextField("Mesajınızı yazın", text: $newMessage)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button(action: sendMessage) {
+                    Text("Gönder")
+                }
+            }
+            .padding()
+        }
+    }
+    
+    func sendMessage() {
+        guard !newMessage.isEmpty else { return }
+        
+        messages.append(Message(content: newMessage, isSentByUser: true))
+        // Burada API çağrısı yaparak gelen mesajı alabilirsiniz.
+        
+        newMessage = ""
     }
 }
+
+
 
 struct ChatAppView_Previews: PreviewProvider {
     static var previews: some View {
